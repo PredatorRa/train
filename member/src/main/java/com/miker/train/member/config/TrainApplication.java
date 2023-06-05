c42d99c8226b37164fb9c45a0513ebc94f2e8d49
PredatorRa <495678026@qq.com>
2023-06-05 14:01:56 +0800

    4.4 新增member会员模块

--- member/src/main/java/com/miker/train/member/config/TrainApplication.java
+++ member/src/main/java/com/miker/train/member/config/TrainApplication.java
@@ -1,9 +1,11 @@
-package com.miker.train;
+package com.miker.train.member.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.miker")
 public class TrainApplication {
 
     public static void main(String[] args) {
--- member/src/main/java/com/miker/train/member/controller/TestController.java
+++ member/src/main/java/com/miker/train/member/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.miker.train;
+package com.miker.train.member.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
