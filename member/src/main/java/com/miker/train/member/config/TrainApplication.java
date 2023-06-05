4832db3fd43a9c1ccdcf8961c75a6eb5de1d1a20
PredatorRa <495678026@qq.com>
2023-06-05 15:53:05 +0800

    4.5 修改启动日志，增加logback，member模块增加前缀/member，gitignore中忽略log日志

--- member/src/main/java/com/miker/train/member/config/TrainApplication.java
+++ member/src/main/java/com/miker/train/member/config/TrainApplication.java
@@ -1,15 +1,22 @@
 package com.miker.train.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.miker")
 public class TrainApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(TrainApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(TrainApplication.class, args);
+        SpringApplication app = new SpringApplication(TrainApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！");
+        LOG.info("测试地址：\thttp://127.0.0.1:{}/member/hello",env.getProperty("server.port"));
     }
 
 }
