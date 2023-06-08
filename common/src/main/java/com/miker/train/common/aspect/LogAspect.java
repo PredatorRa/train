79a8d1c05c3fa51d4f07f48531b420a505e76c49
PredatorRa <495678026@qq.com>
2023-06-08 14:01:33 +0800

    4.8 添加公共模块common，用于放置公共配置、公共类

--- common/src/main/java/com/miker/train/common/aspect/LogAspect.java
+++ common/src/main/java/com/miker/train/common/aspect/LogAspect.java
@@ -1,4 +1,4 @@
-package com.miker.train.member.aspect;
+package com.miker.train.common.aspect;
 
 import cn.hutool.core.util.RandomUtil;
 import com.alibaba.fastjson.JSONObject;
@@ -25,7 +25,7 @@ import org.springframework.web.multipart.MultipartFile;
 @Component
 public class LogAspect {
     public LogAspect() {
-        System.out.println("Common LogAspect");
+        System.out.println("Common1 LogAspect");
     }
 
     private final static Logger LOG = LoggerFactory.getLogger(LogAspect.class);
