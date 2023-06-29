24463de707e093083b86d03632572572cc6e0b48
PredatorRa <495678026@qq.com>
2023-06-29 09:43:13 +0800

    4.9 重命名TrainApplication为MemberApplication

--- member/src/main/java/com/miker/train/member/config/MemberApplication.java
+++ member/src/main/java/com/miker/train/member/config/MemberApplication.java
@@ -9,11 +9,11 @@ import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @ComponentScan("com.miker")
-public class TrainApplication {
-    private static final Logger LOG = LoggerFactory.getLogger(TrainApplication.class);
+public class MemberApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(MemberApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(TrainApplication.class);
+        SpringApplication app = new SpringApplication(MemberApplication.class);
         Environment env = app.run(args).getEnvironment();
         LOG.info("启动成功！");
         LOG.info("测试地址：\thttp://127.0.0.1:{}{}/hello",env.getProperty("server.port"),env.getProperty("server.servlet.context-path"));
