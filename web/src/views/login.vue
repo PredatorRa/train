4c7b1400c27039be1146889a587bd3df0bea05d9
PredatorRa <495678026@qq.com>
2023-07-03 18:54:07 +0800

    5.6 增加login路由及页面

---
+++ web/src/views/login.vue
@@ -0,0 +1,64 @@
+<template>
+    <a-form :layout="formState.layout" :model="formState" v-bind="formItemLayout">
+        <a-form-item label="Form Layout">
+            <a-radio-group v-model:value="formState.layout">
+                <a-radio-button value="horizontal">Horizontal</a-radio-button>
+                <a-radio-button value="vertical">Vertical</a-radio-button>
+                <a-radio-button value="inline">Inline</a-radio-button>
+            </a-radio-group>
+        </a-form-item>
+        <a-form-item label="Field A">
+            <a-input v-model:value="formState.fieldA" placeholder="input placeholder" />
+        </a-form-item>
+        <a-form-item label="Field B">
+            <a-input v-model:value="formState.fieldB" placeholder="input placeholder" />
+        </a-form-item>
+        <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
+            <a-button type="primary">Submit</a-button>
+        </a-form-item>
+    </a-form>
+</template>
+<script>
+import { computed, defineComponent, reactive } from 'vue';
+export default defineComponent({
+    setup() {
+        const formState = reactive({
+            layout: 'horizontal',
+            fieldA: '',
+            fieldB: '',
+        });
+        const formItemLayout = computed(() => {
+            const { layout } = formState;
+            return layout === 'horizontal'
+                ? {
+                    labelCol: {
+                        span: 4,
+                    },
+                    wrapperCol: {
+                        span: 14,
+                    },
+                }
+                : {};
+        });
+        const buttonItemLayout = computed(() => {
+            const { layout } = formState;
+            return layout === 'horizontal'
+                ? {
+                    wrapperCol: {
+                        span: 14,
+                        offset: 4,
+                    },
+                }
+                : {};
+        });
+        return {
+            formState,
+            formItemLayout,
+            buttonItemLayout,
+        };
+    },
+});
+</script>
+<style>
+
+</style>
