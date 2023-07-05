74973610337f89bb51a495e45d064a58b18a855d
PredatorRa <495678026@qq.com>
2023-07-05 09:52:52 +0800

    5.11 VueCLI配置多环境，为axios配置后端域名

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -53,7 +53,7 @@ export default defineComponent({
     });
 
     const sendCode = () => {
-      axios.post("http://localhost:8000/member/member/sendCode",{
+      axios.post("/member/member/sendCode",{
         mobile:loginForm.mobile
       }).then(response=>{
         let data = response.data;
@@ -67,7 +67,7 @@ export default defineComponent({
     };
 
     const login = () => {
-      axios.post("http://localhost:8000/member/member/login",loginForm).then(response=>{
+      axios.post("/member/member/login",loginForm).then(response=>{
         let data = response.data;
         if(data.success){
           notification.success({description:'登录成功！'});
