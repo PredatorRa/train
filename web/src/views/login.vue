e17e1e47f7ff95957ef43c0f3b847299490642d8
Miker <495678026@qq.com>
2023-07-05 23:29:28 +0800

    5.12 增加控台主页，登陆成功后跳转到控台主页

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -43,10 +43,12 @@
 import {defineComponent, reactive} from 'vue';
 import axios from 'axios';
 import {notification} from "ant-design-vue";
+import {useRouter} from 'vue-router';
 
 export default defineComponent({
   name: "login-view",
   setup() {
+    const router = useRouter();
     const loginForm = reactive({
       mobile: '13000000000',
       code: '',
@@ -71,6 +73,8 @@ export default defineComponent({
         let data = response.data;
         if(data.success){
           notification.success({description:'登录成功！'});
+          //登录成功跳转到控台主页
+          router.push("/");
         }else{
           notification.error({description:data.message});
         }
