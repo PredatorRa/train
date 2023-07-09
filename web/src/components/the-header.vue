97cd4c00a4d8a4451d77a2e720f4579024a4eec1
Miker <495678026@qq.com>
2023-07-09 12:47:47 +0800

    6.5 使用stroe保存登录信息

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -1,30 +1,39 @@
 <template>
-    <a-layout-header class="header">
-        <div class="logo"/>
-        <a-menu
-                theme="dark"
-                mode="horizontal"
-                v-model:selectedKeys="selectedKeys1"
-                :style="{ lineHeight: '64px' }"
-        >
-            <a-menu-item key="1">nav 11</a-menu-item>
-            <a-menu-item key="2">nav 2</a-menu-item>
-            <a-menu-item key="3">nav 3</a-menu-item>
-        </a-menu>
-    </a-layout-header>
+  <a-layout-header class="header">
+    <div class="logo"/>
+    <div style="float: right;color: white;">
+      您好：{{member.mobile}}&nbsp;&nbsp;
+      <router-link to="/login">
+        退出登录
+      </router-link>
+    </div>
+    <a-menu
+        theme="dark"
+        mode="horizontal"
+        v-model:selectedKeys="selectedKeys1"
+        :style="{ lineHeight: '64px' }"
+    >
+      <a-menu-item key="1">nav 11</a-menu-item>
+      <a-menu-item key="2">nav 2</a-menu-item>
+      <a-menu-item key="3">nav 3</a-menu-item>
+    </a-menu>
+  </a-layout-header>
 </template>
 
 <script>
 import {defineComponent, ref} from 'vue';
+import store from "@/store";
 
 export default defineComponent({
-    name: "the-header-view",
-    setup() {
+  name: "the-header-view",
+  setup() {
+    let member = store.state.member;
 
-        return {
-            selectedKeys1: ref(['2']),
-        };
-    },
+    return {
+      selectedKeys1: ref(['2']),
+      member,
+    };
+  },
 });
 </script>
 
