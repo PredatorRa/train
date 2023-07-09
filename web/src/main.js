2adefbe40d20309430810cf1a1a8e573c2e4ca12
Miker <495678026@qq.com>
2023-07-10 00:10:19 +0800

    6.9 为axios增加登录相关拦截：请求时带上token，返回时校验返回码是不是401

--- web/src/main.js
+++ web/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 //引入ant-design-vue
-import Antd from 'ant-design-vue';
+import Antd, {notification} from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 //引入ant-design-vue图标组件
 import * as Icons from '@ant-design/icons-vue'
@@ -21,6 +21,12 @@ for(const i in icons){
  */
 axios.interceptors.request.use(function (config) {
     console.log('请求参数：', config);
+    const _token = store.state.member.token;
+    console.log('member信息：',store.state.member);
+    if(_token){
+        config.headers.token = _token;
+        console.log("请求header增加token：",_token);
+    }
     return config;
 }, error => {
     return Promise.reject(error);
@@ -30,6 +36,14 @@ axios.interceptors.response.use(function (response) {
     return response;
 }, error => {
     console.log('返回错误：', error);
+    const response = error.response;
+    const status = response.status;
+    if(status===401){
+        console.log("未登录或登陆超时，跳到登录页");
+        store.commit("setMember",{});
+        notification.error({description:"未登录或登陆超时"});
+        router.push('/login');
+    }
     return Promise.reject(error);
 });
 
