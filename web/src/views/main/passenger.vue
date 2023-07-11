5e0b5b2ac7a037e7c197b22d2a8f490476f7c653
Miker <495678026@qq.com>
2023-07-12 00:20:14 +0800

    7.10 显示分页页码;增加表格分页点击事件;增加刷新按钮，点击查询第1页;增加loading效果;保存成功后刷新列表

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -1,26 +1,33 @@
 <template>
-    <p>
-        <a-button type="primary" @click="showModal">新增</a-button>
-    </p>
-    <a-table :dataSource="passengers" :columns="columns"/>
-    <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk"
-             ok-text="确认" cancel-text="取消">
-        <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
-            <a-form-item label="姓名">
-                <a-input v-model:value="passenger.name"/>
-            </a-form-item>
-            <a-form-item label="身份证">
-                <a-input v-model:value="passenger.idCard"/>
-            </a-form-item>
-            <a-form-item label="类型">
-                <a-select v-model:value="passenger.type">
-                    <a-select-option value="1">成人</a-select-option>
-                    <a-select-option value="2">儿童</a-select-option>
-                    <a-select-option value="3">学生</a-select-option>
-                </a-select>
-            </a-form-item>
-        </a-form>
-    </a-modal>
+  <p>
+    <a-space>
+      <a-button type="primary" @click="handleQuery()">刷新</a-button>
+      <a-button type="primary" @click="showModal">新增</a-button>
+    </a-space>
+  </p>
+  <a-table :dataSource="passengers"
+           :columns="columns"
+           :pagination="pagination"
+           @change="handleTableChange"
+           :loading="loading"/>
+  <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk"
+           ok-text="确认" cancel-text="取消">
+    <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+      <a-form-item label="姓名">
+        <a-input v-model:value="passenger.name"/>
+      </a-form-item>
+      <a-form-item label="身份证">
+        <a-input v-model:value="passenger.idCard"/>
+      </a-form-item>
+      <a-form-item label="类型">
+        <a-select v-model:value="passenger.type">
+          <a-select-option value="1">成人</a-select-option>
+          <a-select-option value="2">儿童</a-select-option>
+          <a-select-option value="3">学生</a-select-option>
+        </a-select>
+      </a-form-item>
+    </a-form>
+  </a-modal>
 </template>
 <script>
 import {defineComponent, reactive, ref, onMounted} from 'vue';
@@ -28,83 +35,115 @@ import axios from "axios";
 import {notification} from "ant-design-vue";
 
 export default defineComponent({
-    setup() {
-        const visible = ref(false);
+  setup() {
+    const visible = ref(false);
 
-        const passenger = reactive({
-            id: undefined,
-            memberId: undefined,
-            name: undefined,
-            idCard: undefined,
-            type: undefined,
-            createTime: undefined,
-            updateTime: undefined,
-        });
+    const passenger = reactive({
+      id: undefined,
+      memberId: undefined,
+      name: undefined,
+      idCard: undefined,
+      type: undefined,
+      createTime: undefined,
+      updateTime: undefined,
+    });
 
-        const passengers = ref([]);
+    const passengers = ref([]);
+    let loading = ref(false);
+    const pagination = reactive({
+      total: 0,
+      current: 1,
+      pageSize: 2,
+    });
 
-        const columns = [{
-            title: '姓名',
-            dataIndex: 'name',
-            key: 'name',
-        }, {
-            title: '身份证',
-            dataIndex: 'idCard',
-            key: 'idCard',
-        }, {
-            title: '类型',
-            dataIndex: 'idCard',
-            key: 'idCard',
-        }];
+    const columns = [{
+      title: '姓名',
+      dataIndex: 'name',
+      key: 'name',
+    }, {
+      title: '身份证',
+      dataIndex: 'idCard',
+      key: 'idCard',
+    }, {
+      title: '类型',
+      dataIndex: 'idCard',
+      key: 'idCard',
+    }];
 
-        const showModal = () => {
-            visible.value = true;
+    const showModal = () => {
+      visible.value = true;
+    };
+
+    const handleOk = () => {
+      axios.post("/member/passenger/save", passenger).then((response) => {
+        let data = response.data;
+        if (data.success) {
+          notification.success({description: "保存成功！"});
+          visible.value = false;
+          handleQuery({
+            page: pagination.current,
+            size: pagination.pageSize
+          });
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
+
+    const handleQuery = (param) => {
+      if (!param) {
+        param = {
+          page: 1,
+          size: pagination.pageSize
         };
+      }
+      loading.value = true;
+      axios.get("/member/passenger/query-list", {
+        params: {
+          page: param.page,
+          size: param.size
+        }
+      }).then((response) => {
+        loading.value = false;
+        let data = response.data;
+        if (data.success) {
+          passengers.value = data.content.list;
+          pagination.current = param.page;
+          pagination.total = data.content.total;
+        } else {
+          notification.error({description: data.message});
+        }
+      });
+    };
 
-        const handleOk = () => {
-            axios.post("/member/passenger/save", passenger).then((response) => {
-                let data = response.data;
-                if (data.success) {
-                    notification.success({description: "保存成功！"});
-                    visible.value = false;
-                } else {
-                    notification.error({description: data.message});
-                }
-            });
-        };
+    const handleTableChange = (pagination) => {
+      // console.log("看看自带的分页参数都有啥：" + pagination);
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      });
+    };
 
-        const handleQuery = (param) => {
-            axios.get("/member/passenger/query-list", {
-                params: {
-                    page: param.page,
-                    size: param.size
-                }
-            }).then((response) => {
-                let data = response.data;
-                if (data.success) {
-                    passengers.value = data.content.list;
-                } else {
-                    notification.error({description: data.message});
-                }
-            });
-        };
+    onMounted(() => {
+      handleQuery({
+        page: 1,
+        size: pagination.pageSize
+      })
+    })
 
-        onMounted(() => {
-            handleQuery({
-                page: 1,
-                size: 2
-            })
-        })
-
-        return {
-            passenger,
-            visible,
-            showModal,
-            handleOk,
-            passengers,
-            columns
-        };
-    },
+    return {
+      passenger,
+      visible,
+      showModal,
+      handleOk,
+      passengers,
+      pagination,
+      columns,
+      handleTableChange,
+      handleQuery,
+      loading
+    };
+  },
 });
 </script>
 <style>
