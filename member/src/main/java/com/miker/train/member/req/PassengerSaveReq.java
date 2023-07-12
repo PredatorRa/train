8b974f7b1cf2e0e9a4ba969cf0cd01a9286788cb
PredatorRa <495678026@qq.com>
2023-07-12 19:51:55 +0800

    7.13 乘车人编辑界面开发，reactive改为ref，reactive需要再封装一层属性才能实现双向绑定；解决没保存也会同步修改列表的问题

--- member/src/main/java/com/miker/train/member/req/PassengerSaveReq.java
+++ member/src/main/java/com/miker/train/member/req/PassengerSaveReq.java
@@ -1,7 +1,7 @@
 package com.miker.train.member.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.Date;
 
@@ -19,8 +19,10 @@ public class PassengerSaveReq {
     @NotBlank(message = "【旅客类型】不能为空")
     private String type;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;
 
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date updateTime;
 
     public Long getId() {
