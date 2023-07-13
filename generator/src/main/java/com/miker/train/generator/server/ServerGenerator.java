63a7837a0433634d5dbe7f46fe731fe01c23feb2
Miker <495678026@qq.com>
2023-07-13 23:43:10 +0800

    8.4 集成dom4j，读取pom.xml，得到当前持久层的xml文件;读取当前持久层的xml文件，得到表名和实体名

--- generator/src/main/java/com/miker/train/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/miker/train/generator/server/ServerGenerator.java
@@ -2,7 +2,12 @@ package com.miker.train.generator.server;
 
 import com.miker.train.generator.util.FreemarkerUtil;
 import freemarker.template.TemplateException;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Node;
+import org.dom4j.io.SAXReader;
 
+import javax.xml.parsers.SAXParser;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
@@ -10,14 +15,31 @@ import java.util.Map;
 
 public class ServerGenerator {
     static String toPath = "generator\\src\\main\\java\\com\\miker\\train\\generator\\test\\";
+
+    static String pomPath = "generator/pom.xml";
     static {
         new File(toPath).mkdirs();
     }
 
-    public static void main(String[] args) throws IOException, TemplateException {
-        FreemarkerUtil.initConfig("test.ftl");
-        Map<String, Object> param = new HashMap<>();
-        param.put("domain", "Test1");
-        FreemarkerUtil.generator(toPath + "Test1.java", param);
+    public static void main(String[] args) throws Exception {
+        String generatorPath = getGeneratorPath();
+
+        SAXReader saxReader = new SAXReader();
+        Document document = saxReader.read("generator/"+generatorPath);
+        Node table = document.selectSingleNode("//table");
+        Node tableName = table.selectSingleNode("@tableName");
+        Node domainObjectName = table.selectSingleNode("@domainObjectName");
+        System.out.println(tableName.getText()+"/"+domainObjectName.getText());
+    }
+
+    public static String getGeneratorPath() throws Exception {
+        SAXReader saxReader = new SAXReader();
+        Map<String, String> map = new HashMap<>();
+        map.put("pom","http://maven.apache.org/POM/4.0.0");
+        saxReader.getDocumentFactory().setXPathNamespaceURIs(map);
+        Document document = saxReader.read(pomPath);
+        Node node = document.selectSingleNode("//pom:configurationFile");
+        System.out.println(node.getText());
+        return node.getText();
     }
 }
