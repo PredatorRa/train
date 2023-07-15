56eaca1400a91539c8353e2dabad38646959f94d
Miker <495678026@qq.com>
2023-07-15 13:25:55 +0800

    9.3 创建admin模块，所有的代码都是从web复制；admin去掉登录页面，去掉member和登录相关的拦截，去掉乘车人页面；去掉没用的HelloWorld组件，修改logo区域

---
+++ admin/src/components/the-sider.vue
@@ -0,0 +1,46 @@
+<template>
+  <a-layout-sider width="200" style="background: #fff">
+    <a-menu
+        v-model:selectedKeys="selectedKeys"
+        mode="inline"
+        :style="{ height: '100%', borderRight: 0 }"
+    >
+      <a-menu-item key="/welcome">
+        <router-link to="/welcome">
+          <coffee-outlined /> &nbsp; 欢迎
+        </router-link>
+      </a-menu-item>
+      <a-menu-item key="/about">
+        <router-link to="/about">
+          <user-outlined /> &nbsp; 关于
+        </router-link>
+      </a-menu-item>
+    </a-menu>
+  </a-layout-sider>
+</template>
+
+<script>
+import {defineComponent, ref, watch} from 'vue';
+import router from "@/router";
+
+export default defineComponent({
+  name: "the-sider-view",
+  setup() {
+    const selectedKeys = ref([]);
+
+    watch(() => router.currentRoute.value.path, (newValue) => {
+      console.log('watch', newValue);
+      selectedKeys.value = [];
+      selectedKeys.value.push(newValue);
+    }, {immediate: true});
+    return {
+      selectedKeys
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
