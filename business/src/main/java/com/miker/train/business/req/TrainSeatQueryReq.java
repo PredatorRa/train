f0c06b154faaaa1fd681915fcfcd60210cb42470
Miker <495678026@qq.com>
2023-07-16 13:10:37 +0800

    9.10 增加火车座位表，生成持久层代码及前后端代码

---
+++ business/src/main/java/com/miker/train/business/req/TrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package com.miker.train.business.req;
+
+import com.miker.train.common.req.PageReq;
+
+public class TrainSeatQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainSeatQueryReq{" +
+                "} " + super.toString();
+    }
+}
--- generator/src/main/java/com/miker/train/generator/gen/EnumGenerator.java
+++ generator/src/main/java/com/miker/train/generator/gen/EnumGenerator.java
@@ -2,6 +2,7 @@ package com.miker.train.generator.gen;
 
 
 import cn.hutool.core.util.StrUtil;
+import com.miker.train.business.enums.SeatColEnum;
 import com.miker.train.business.enums.SeatTypeEnum;
 import com.miker.train.business.enums.TrainTypeEnum;
 import com.miker.train.member.enums.PassengerTypeEnum;
@@ -21,6 +22,7 @@ public class EnumGenerator {
             toJson(PassengerTypeEnum.class, bufferObject, bufferArray);
             toJson(TrainTypeEnum.class, bufferObject, bufferArray);
             toJson(SeatTypeEnum.class, bufferObject, bufferArray);
+            toJson(SeatColEnum.class, bufferObject, bufferArray);
 
             StringBuffer buffer = bufferObject.append("\r\n").append(bufferArray);
             writeJs(buffer);
