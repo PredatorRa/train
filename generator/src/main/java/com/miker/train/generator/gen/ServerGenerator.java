ebc3d5646a5ceba3741f50e14535242615147fbd
Miker <495678026@qq.com>
2023-07-16 21:19:48 +0800

    9.17 重新生成火车座位页面，只读

--- generator/src/main/java/com/miker/train/generator/gen/ServerGenerator.java
+++ generator/src/main/java/com/miker/train/generator/gen/ServerGenerator.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.util.*;
 
 public class ServerGenerator {
-    static boolean readOnly = false;
+    static boolean readOnly = true;
     static String vuePath = "admin/src/views/main/";
     static String serverPath = "[module]/src/main/java/com/miker/train/[module]/";
     static String pomPath = "generator/pom.xml";
@@ -70,11 +70,11 @@ public class ServerGenerator {
         param.put("readOnly", readOnly);
         System.out.println("组装参数：" + param);
 
-         gen(Domain, param, "service", "service");
-         gen(Domain, param, "controller/admin", "adminController");
-         gen(Domain, param, "req", "saveReq");
-         gen(Domain, param, "req", "queryReq");
-         gen(Domain, param, "resp", "queryResp");
+//         gen(Domain, param, "service", "service");
+//         gen(Domain, param, "controller/admin", "adminController");
+//         gen(Domain, param, "req", "saveReq");
+//         gen(Domain, param, "req", "queryReq");
+//         gen(Domain, param, "resp", "queryResp");
 
 
         genVue(do_main, param);
