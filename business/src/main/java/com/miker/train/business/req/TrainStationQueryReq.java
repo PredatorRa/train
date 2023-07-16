f8f1fe4ab8922f87fb588d8fd29617094cbbb9fd
Miker <495678026@qq.com>
2023-07-16 12:28:54 +0800

    9.8 增加火车车站表，生成持久层代码及前后端代码

---
+++ business/src/main/java/com/miker/train/business/req/TrainStationQueryReq.java
@@ -0,0 +1,12 @@
+package com.miker.train.business.req;
+
+import com.miker.train.common.req.PageReq;
+
+public class TrainStationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainStationQueryReq{" +
+                "} " + super.toString();
+    }
+}
