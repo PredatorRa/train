57bfe441f1a6604ea2252e15c72a2f9bdef9dec0
PredatorRa <495678026@qq.com>
2023-07-17 18:51:06 +0800

    11.5 快速生成每日车厢数据管理功能

---
+++ business/src/main/java/com/miker/train/business/req/DailyTrainCarriageQueryReq.java
@@ -0,0 +1,12 @@
+package com.miker.train.business.req;
+
+import com.miker.train.common.req.PageReq;
+
+public class DailyTrainCarriageQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainCarriageQueryReq{" +
+                "} " + super.toString();
+    }
+}
