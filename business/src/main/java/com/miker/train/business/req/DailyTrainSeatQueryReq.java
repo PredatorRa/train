7ae6676f41ea91102df8fe76b106961ddba78432
Miker <495678026@qq.com>
2023-07-17 23:36:14 +0800

    11.6 增加每日座位表，生成前后端代码

---
+++ business/src/main/java/com/miker/train/business/req/DailyTrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package com.miker.train.business.req;
+
+import com.miker.train.common.req.PageReq;
+
+public class DailyTrainSeatQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainSeatQueryReq{" +
+                "} " + super.toString();
+    }
+}
