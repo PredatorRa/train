0ed35b00d7a3a8314f58e9b6158a1d70b3d403ff
Miker <495678026@qq.com>
2023-07-18 01:27:15 +0800

    11.10 增加生成每日车站功能

--- business/src/main/java/com/miker/train/business/service/DailyTrainService.java
+++ business/src/main/java/com/miker/train/business/service/DailyTrainService.java
@@ -3,6 +3,7 @@ package com.miker.train.business.service;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
@@ -34,6 +35,9 @@ public class DailyTrainService {
 
     @Resource
     private TrainService trainService;
+
+    @Resource
+    private DailyTrainStationService dailyTrainStationService;
 
     public void save(DailyTrainSaveReq req) {
         DateTime now = DateTime.now();
@@ -96,6 +100,8 @@ public class DailyTrainService {
     }
 
     public void genDailyTrain(Date date, Train train) {
+        LOG.info("生成日期【{}】车次【{}】的信息开始", DateUtil.formatDate(date), train.getCode());
+
         //删除原有车次
         DailyTrainExample deleteExample = new DailyTrainExample();
         deleteExample.createCriteria()
@@ -111,6 +117,11 @@ public class DailyTrainService {
         dailyTrain.setUpdateTime(now);
         dailyTrain.setDate(date);
         dailyTrainMapper.insert(dailyTrain);
+
+        // 生成该车次的车站数据
+        dailyTrainStationService.genDaily(date, train.getCode());
+        LOG.info("生成日期【{}】车次【{}】的信息结束", DateUtil.formatDate(date), train.getCode());
+
     }
 
 }
