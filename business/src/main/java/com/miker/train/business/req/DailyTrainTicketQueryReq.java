a1126b6473e84ac4ed1a7f95d1f7274183ef70a6
PredatorRa <495678026@qq.com>
2023-07-18 10:09:49 +0800

    12.2 增加余票信息表，生成前后端代码

---
+++ business/src/main/java/com/miker/train/business/req/DailyTrainTicketQueryReq.java
@@ -0,0 +1,12 @@
+package com.miker.train.business.req;
+
+import com.miker.train.common.req.PageReq;
+
+public class DailyTrainTicketQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainTicketQueryReq{" +
+                "} " + super.toString();
+    }
+}
