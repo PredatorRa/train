a4598a1bb56f311d350af82a3b8e616463d4c8ed
didi <mikerchen_i@didiglobal.com>
2023-07-19 15:13:07 +0800

    12.8 订票页面，优化购票列表的展示

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -23,6 +23,32 @@
   选中的乘客：{{passengerChecks}}
   <br/>
   购票列表：{{tickets}}
+  <div class="order-tickets">
+    <a-row class="order-tickets-header" v-if="tickets.length > 0">
+      <a-col :span="2">乘客</a-col>
+      <a-col :span="6">身份证</a-col>
+      <a-col :span="4">票种</a-col>
+      <a-col :span="4">座位类型</a-col>
+    </a-row>
+    <a-row class="order-tickets-row" v-for="ticket in tickets" :key="ticket.passengerId">
+      <a-col :span="2">{{ticket.passengerName}}</a-col>
+      <a-col :span="6">{{ticket.passengerIdCard}}</a-col>
+      <a-col :span="4">
+        <a-select v-model:value="ticket.passengerType" style="width: 100%">
+          <a-select-option v-for="item in PASSENGER_TYPE_ARRAY" :key="item.code" :value="item.code">
+            {{item.desc}}
+          </a-select-option>
+        </a-select>
+      </a-col>
+      <a-col :span="4">
+        <a-select v-model:value="ticket.seatTypeCode" style="width: 100%">
+          <a-select-option v-for="item in seatTypes" :key="item.code" :value="item.code">
+            {{item.desc}}
+          </a-select-option>
+        </a-select>
+      </a-col>
+    </a-row>
+  </div>
 </template>
 
 <script>
@@ -65,7 +91,6 @@ export default defineComponent({
       }
     }
     console.log("本车次提供的座位：", seatTypes)
-
     // 购票列表，用于界面展示，并传递到后端接口，用来描述：哪个乘客购买什么座位的票
     // {
     //   passengerId: 123,
@@ -75,6 +100,7 @@ export default defineComponent({
     //   seatTypeCode: "1"
     // }
     const tickets = ref([]);
+    const PASSENGER_TYPE_ARRAY = window.PASSENGER_TYPE_ARRAY;
 
     // 勾选或去掉某个乘客时，在购票列表中加上或去掉一张表
     watch(() => passengerChecks.value, (newVal, oldVal)=>{
@@ -115,7 +141,8 @@ export default defineComponent({
       seatTypes,
       passengerOptions,
       passengerChecks,
-      tickets
+      tickets,
+      PASSENGER_TYPE_ARRAY
     };
   },
 });
@@ -132,5 +159,25 @@ export default defineComponent({
 .order-train .order-train-ticket .order-train-ticket-main {
   color: red;
   font-size: 18px;
+}
+
+.order-tickets {
+  margin: 10px 0;
+}
+.order-tickets .ant-col {
+  padding: 5px 10px;
+}
+.order-tickets .order-tickets-header {
+  background-color: cornflowerblue;
+  border: solid 1px cornflowerblue;
+  color: white;
+  font-size: 16px;
+  padding: 5px 0;
+}
+.order-tickets .order-tickets-row {
+  border: solid 1px cornflowerblue;
+  border-top: none;
+  vertical-align: middle;
+  line-height: 30px;
 }
 </style>
