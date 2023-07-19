8f905cc621f521b5ab308b2855e64a463ee01c72
Miker <495678026@qq.com>
2023-07-19 22:35:19 +0800

    12.11 增加确认订单表，生成前后端代码

---
+++ business/src/main/java/com/miker/train/business/req/ConfirmOrderQueryReq.java
@@ -0,0 +1,12 @@
+package com.miker.train.business.req;
+
+import com.miker.train.common.req.PageReq;
+
+public class ConfirmOrderQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "ConfirmOrderQueryReq{" +
+                "} " + super.toString();
+    }
+}
--- generator/src/main/java/com/miker/train/generator/gen/EnumGenerator.java
+++ generator/src/main/java/com/miker/train/generator/gen/EnumGenerator.java
@@ -2,6 +2,7 @@ package com.miker.train.generator.gen;
 
 
 import cn.hutool.core.util.StrUtil;
+import com.miker.train.business.enums.ConfirmOrderStatusEnum;
 import com.miker.train.business.enums.SeatColEnum;
 import com.miker.train.business.enums.SeatTypeEnum;
 import com.miker.train.business.enums.TrainTypeEnum;
@@ -17,7 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EnumGenerator {
-    static String path = "web/src/assets/js/enums.js";
+    static String path = "admin/src/assets/js/enums.js";
 
     public static void main(String[] args) {
         StringBuffer bufferObject = new StringBuffer();
@@ -28,6 +29,8 @@ public class EnumGenerator {
             toJson(TrainTypeEnum.class, bufferObject, bufferArray);
             toJson(SeatTypeEnum.class, bufferObject, bufferArray);
             toJson(SeatColEnum.class, bufferObject, bufferArray);
+            toJson(ConfirmOrderStatusEnum.class, bufferObject, bufferArray);
+
 
             StringBuffer buffer = bufferObject.append("\r\n").append(bufferArray);
             writeJs(buffer);
