f81ba00c8cc3b33c1c0d5b75556d03aabbed21f0
Miker <495678026@qq.com>
2023-07-22 19:15:42 +0800

    12.22 增加乘客车票表，生成前后端代码

---
+++ member/src/main/java/com/miker/train/member/controller/admin/TicketAdminController.java
@@ -0,0 +1,39 @@
+package com.miker.train.member.controller.admin;
+
+import com.miker.train.common.context.LoginMemberContext;
+import com.miker.train.common.resp.CommonResp;
+import com.miker.train.common.resp.PageResp;
+import com.miker.train.member.req.TicketQueryReq;
+import com.miker.train.member.req.TicketSaveReq;
+import com.miker.train.member.resp.TicketQueryResp;
+import com.miker.train.member.service.TicketService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/ticket")
+public class TicketAdminController {
+
+    @Resource
+    private TicketService ticketService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody TicketSaveReq req) {
+        ticketService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<TicketQueryResp>> queryList(@Valid TicketQueryReq req) {
+        PageResp<TicketQueryResp> list = ticketService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        ticketService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
---
+++ member/src/main/java/com/miker/train/member/req/TicketQueryReq.java
@@ -0,0 +1,12 @@
+package com.miker.train.member.req;
+
+import com.miker.train.common.req.PageReq;
+
+public class TicketQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TicketQueryReq{" +
+                "} " + super.toString();
+    }
+}
