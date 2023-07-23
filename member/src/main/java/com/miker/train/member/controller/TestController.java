c1cd975767488606fe6c56e4064b03cfb945718b
Miker <495678026@qq.com>
2023-07-23 11:43:25 +0800

    13.4 读取配置文件的配置

--- member/src/main/java/com/miker/train/member/controller/TestController.java
+++ member/src/main/java/com/miker/train/member/controller/TestController.java
@@ -1,12 +1,17 @@
 package com.miker.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.nacos}")
+    private String testNacos;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World12312312";
+        return String.format("Hello %s!", testNacos);
     }
 }
