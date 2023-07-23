afeffa74d5967480bc4725bbb04abfcb962dc3df
Miker <495678026@qq.com>
2023-07-23 21:38:46 +0800

    13.8 配置Gateway+Nacos，实现按应用名来做路由转发

--- member/src/main/java/com/miker/train/member/controller/TestController.java
+++ member/src/main/java/com/miker/train/member/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.miker.train.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,12 @@ public class TestController {
     @Value("${test.nacos}")
     private String testNacos;
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/hello")
     public String hello(){
-        return String.format("Hello %s!", testNacos);
+        String port = environment.getProperty("local.server.port");
+        return String.format("Hello %s! 端口：%s", testNacos, port);
     }
 }
