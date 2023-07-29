c4da0112b5ccf0a65cc552d3cbc4c4aa5cc46aaf
Miker <495678026@qq.com>
2023-07-29 17:39:03 +0800

    17.8 修改sentinel与nacos相关配置

--- business/src/main/java/com/miker/train/business/controller/TestController.java
+++ business/src/main/java/com/miker/train/business/controller/TestController.java
@@ -1,10 +1,13 @@
 package com.miker.train.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @SentinelResource("hello")
     @GetMapping("/hello")
     public String hello() {
         return "Hello World! Business!";
