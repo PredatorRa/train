085c588685ec2e96afcefc9e182e020276cfe1c1
Miker <495678026@qq.com>
2023-08-06 13:39:52 +0800

    22.5 座位销售图页面获得销售信息，同一趟车，不管查哪个区间，查到的销售信息是一样的，由界面再去截取区间的销售信息。功能设计经验：对于复杂的操作，能放到前端的都放到前端，减小后端的压力。

--- web/src/views/main/seat.vue
+++ web/src/views/main/seat.vue
@@ -1,32 +1,64 @@
 <template>
-  <div v-if="!param.date">
-    请到余票查询里选择一趟列车，
-    <router-link to="/ticket">
-      跳转到余票查询
-    </router-link>
-  </div>
-  <div v-else>
-    <p>
-      日期：{{param.date}}，车次：{{param.trainCode}}，出发站：{{param.start}}，到达站：{{param.end}}
-    </p>
-  </div>
+    <div v-if="!param.date">
+        请到余票查询里选择一趟列车，
+        <router-link to="/ticket">
+            跳转到余票查询
+        </router-link>
+    </div>
+    <div v-else>
+        <p>
+            日期：{{ param.date }}，车次：{{ param.trainCode }}，出发站：{{ param.start }}，到达站：{{ param.end }}
+        </p>
+        <p>
+            {{ list }}
+        </p>
+    </div>
 </template>
 
 <script>
 
-import { defineComponent, ref } from 'vue';
+import { defineComponent, ref, onMounted } from 'vue';
+import axios from "axios";
+import {notification} from "ant-design-vue";
 import {useRoute} from "vue-router";
 
-export default defineComponent({
-  name: "welcome-view",
-  setup() {
-    const route = useRoute();
-    const param = ref({});
-    param.value = route.query;
 
-    return {
-      param
-    };
-  },
+export default defineComponent({
+    name: "welcome-view",
+    setup() {
+        const route = useRoute();
+        const param = ref({});
+        param.value = route.query;
+        const list = ref();
+
+        // 查询一列火车的所有销售信息
+        const querySeat = () => {
+            axios.get("/business/seat-sell/query", {
+                params: {
+                    date: param.value.date,
+                    trainCode: param.value.trainCode,
+                }
+            }).then((response) => {
+                let data = response.data;
+                if (data.success) {
+                    list.value = data.content;
+                } else {
+                    notification.error({description: data.message});
+                }
+            });
+        };
+
+        onMounted(() => {
+            if (param.value.date) {
+                querySeat();
+            }
+        });
+
+        return {
+            param,
+            querySeat,
+            list
+        };
+    },
 });
 </script>
